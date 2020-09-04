ed8920436a6aa231f344b0c4b4bbea129c7bfe80
Bryan Bowie <PolarBearGod@users.noreply.github.com>
2020-09-04 11:50:53 -0400

    Create Get-ieExtension.ps1

---
+++ Get-ieExtension.ps1
@@ -0,0 +1,3 @@
+If (!(Test-Path HKCR:)){New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT}$searchScopes = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Ext\Settings","HKLM:\Software\Microsoft\Windows\CurrentVersion\explorer\Browser Helper Objects"; $searchScopes | Get-ChildItem -Recurse | Select -ExpandProperty PSChildName | ForEach-Object {If(Test-Path "HKCR:\CLSID\$_"){Get-ItemProperty -Path "HKCR:\CLSID\$_" | Select-Object @{n="Name";e="(default)"}}} | Sort-Object -Unique -Property Name
+# Delete this line and the comment hashes to have this export the contents to the host.
+# | Export-Csv -Path "c:\$env:COMPUTERNAME-ieExtensions.csv" -NoTypeInformation
