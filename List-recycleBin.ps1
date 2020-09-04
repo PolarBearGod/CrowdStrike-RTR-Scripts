9da8f0c51b6792583038b779ea31c8831bd66664
Bryan Bowie <PolarBearGod@users.noreply.github.com>
2020-09-04 12:03:04 -0400

    Update List-recycleBin.ps1

--- List-recycleBin.ps1
+++ List-recycleBin.ps1
@@ -1,1 +1,3 @@
-  (New-Object -ComObject Shell.Application).NameSpace(0x0a).Items() | select @{n="OriginalLocation"; e={$_.ExtendedProperty("{9B174B33-40FF-11D2-A27E-00C04FC30871} 2")}},Name;
+(New-Object -ComObject Shell.Application).NameSpace(0x0a).Items() | select @{n="OriginalLocation"; e={$_.ExtendedProperty("{9B174B33-40FF-11D2-A27E-00C04FC30871} 2")}},Name;
+# Delete this line and the comment hashes to have this export the contents to the host.
+# | Export-Csv -Path "c:\$env:COMPUTERNAME-recyclebin.csv" -NoTypeInformation
