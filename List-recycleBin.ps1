a2e3c7e0cafbb73d87a24b7f5f4a8da4996dd396
Bryan Bowie <PolarBearGod@users.noreply.github.com>
2021-08-05 18:07:46 -0400

    Update List-recycleBin.ps1
    Added a caution mess age about this as it doesn't work as intended through RTR.

--- List-recycleBin.ps1
+++ List-recycleBin.ps1
@@ -1,3 +1,6 @@
 (New-Object -ComObject Shell.Application).NameSpace(0x0a).Items() | select @{n="OriginalLocation"; e={$_.ExtendedProperty("{9B174B33-40FF-11D2-A27E-00C04FC30871} 2")}},Name;
 # Delete this line and the comment hashes to have this export the contents to the host.
 # | Export-Csv -Path "c:\$env:COMPUTERNAME-recyclebin.csv" -NoTypeInformation
+
+# Note: this command does not work 100% directly in RTR. When RTR runs, it is in SYSTEM context which likely has nothing in its trash bin.
+# The above scriptblock is best used on a live system or directly into a script file and executed under a user's context (i.e. New-PSSession)
